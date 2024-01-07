b42486f282f77bfafbb37643d64c08ac59d6f8b6
Joel Löf <jlof@evolution.com>
2024-01-07 20:26:14 +0100

    chore: update server

--- serve.mjs
+++ serve.mjs
@@ -2,17 +2,11 @@
 
 import cors from 'cors';
 import express from 'express';
-import fs from 'fs';
 import http from 'http';
-import https from 'https';
-import { resolve } from 'path';
 import { Server } from 'socket.io';
 
 const httpApp = express();
 httpApp.use(cors());
-
-// const sslApp = express();
-// sslApp.use(cors());
 
 try {
   start(
@@ -23,35 +17,19 @@ try {
 } catch (e) {
   console.error(e)
 }
-try {
-  // start(https.createServer({
-  //   // key: fs.readFileSync(file`private.key`),
-  //   key: fs.readFileSync('/etc/letsencrypt/live/streams.autos/privkey.pem'),
-  //   cert: fs.readFileSync('/etc/letsencrypt/live/streams.autos/fullchain.pem'),
-  // }, sslApp),
-  // sslApp,
-  //   8443
-  // );
-} catch (e) {
-  console.error(e)
-}
 
 /**
- * @param {http.Server | https.Server} server
+ * @param {http.Server} server
  * @param {express.Express} app
  * @param {number} port
  * @returns {void}
  */
 function start(server, app, port) {
   const io = new Server(server);
-  const validation = file`11A934882D5862AF9BBFEDE453D6433F.txt`
 
   app.get('/', (req, res) => {
     res.sendFile(file`index.html`);
   });
-  app.get(`/.well-known/pki-validation/${validation}`, (req, res) => {
-    res.sendFile(validation);
-});
 
   io.on('connection', (socket) => {
     // Handle WebRTC signaling
@@ -74,18 +52,4 @@ function start(server, app, port) {
   server.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
   });
-}
-
-/**
- * Get the absolute path of a file
- * @param {string[]} strings
- * @param {...*} values
- * @returns {string}
- **/
-function file(strings, ...values) {
-  let result = "";
-  strings.forEach((string, i) => {
-      result += string + (values[i] || '');
-  });
-  return resolve(resolve(), result);
 }
